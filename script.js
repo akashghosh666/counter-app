e92fe38bdba7408289e9e7351da18d2baddceec0
Akash Ghosh <ag0406219@gmail.com>
2025-10-08 12:58:12 +0530

    change variable position

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let count = 0;
 const countNum = document.getElementById("count");
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const resetBtn = document.getElementById("reset");
 
+let count = 0;
+
 function update(val = 0) {
-    count = val === "reset" ? 0 : count + val;
-    countNum.innerHTML = count;
-    countNum.style.color = count > 0 ? "green" : count < 0 ? "red" : "black";
+  count = val === "reset" ? 0 : count + val;
+  countNum.innerHTML = count;
+  countNum.style.color = count > 0 ? "green" : count < 0 ? "red" : "black";
 }
 
 increaseBtn.addEventListener("click", () => update(1));
